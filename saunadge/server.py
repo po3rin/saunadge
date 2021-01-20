7fc29270c2ef2dc645dc49060da2f0e954574a9f
中村 弘武 <abctail30@gmail.com>
2021-01-20 23:52:18 +0900

    first commit

---
+++ saunadge/server.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask
+
+app = Flask(__name__)
+
+BASE_URL = "https://sauna-ikitai.com/saunners/"
+
+
+@app.route("/api/v1/badge/<int:user_id>")
+def tonttu_badge(user_id):
+    res = requests.get(BASE_URL + f"{user_id}")
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    sakatsu = (soup.find('body').find('div', class_='l-containers js-containers').find(
+        'div', class_='l-content is-noPaddingBottom').find('div', class_='p-mypage').find("ul", class_="p-localNav_links js-swipeScrollInner is-noborder")
+        .find_all("li", class_="p-localNav_link")[0].find("span")
+        .find("span", class_="p-localNav_count").get_text().strip()
+    )
+    return {
+        "schemaVersion": 1,
+        "label": int(sakatsu),
+        "message": sakatsu,
+        "color": "0051e0",
+    }
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
